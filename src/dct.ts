f58d85135fc37bdaba930feff33e7231bf6901cd
swnb <swnblovego@gmail.com>
2022-01-26 16:12:26 +0800

    perf(dct)

--- src/dct.ts
+++ src/dct.ts
@@ -16,6 +16,11 @@ export function dct(matrix: number[][]): number[][] {
     result[i] = Array(n)
   }
 
+  const c1m = Math.sqrt(1 / m)
+  const c2m = Math.sqrt(2 / m)
+  const c1n = Math.sqrt(1 / n)
+  const c2n = Math.sqrt(2 / n)
+
   for (let u = 0; u < m; u++) {
     for (let v = 0; v < n; v++) {
       let value = 0
@@ -28,8 +33,9 @@ export function dct(matrix: number[][]): number[][] {
             Math.cos((Math.PI * v * (y + 0.5)) / n)
         }
       }
-      const arg1 = Math.sqrt((u === 0 ? 1 : 2) / m)
-      const arg2 = Math.sqrt((v === 0 ? 1 : 2) / n)
+
+      const arg1 = u === 0 ? c1m : c2m
+      const arg2 = v === 0 ? c1n : c2n
 
       result[u][v] = arg1 * arg2 * value
     }
@@ -56,6 +62,11 @@ export function idct(matrix: number[][]) {
     result[i] = Array(n)
   }
 
+  const c1m = Math.sqrt(1 / m)
+  const c2m = Math.sqrt(2 / m)
+  const c1n = Math.sqrt(1 / n)
+  const c2n = Math.sqrt(2 / n)
+
   for (let x = 0; x < m; x++) {
     for (let y = 0; y < n; y++) {
       let value = 0
@@ -63,8 +74,8 @@ export function idct(matrix: number[][]) {
         for (let v = 0; v < n; v++) {
           const preValue = matrix[u][v]
 
-          const arg1 = Math.sqrt((u === 0 ? 1 : 2) / m)
-          const arg2 = Math.sqrt((v === 0 ? 1 : 2) / n)
+          const arg1 = u === 0 ? c1m : c2m
+          const arg2 = v === 0 ? c1n : c2n
 
           value +=
             arg1 *
